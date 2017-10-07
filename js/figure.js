893453fa48024be956f0ef46f1e508c37c6f7bf0
Edward Paget <edward@cassetteta.pe>
2017-10-07 19:08:22 -0400

    Start working on figure pop out

---
+++ js/figure.js
@@ -0,0 +1,22 @@
+(function() {
+
+    function handleInfo(el) {
+        var span = document.createElement('span'),
+            infoBubble = document.create
+        span.style = {
+            "position": "aboslute",
+            "right": "0.5vw",
+            "bottom": "0.5vw"
+        }
+
+        el.appendCHild
+    }
+
+    function initialize() {
+        this.document.querySelectorAll('.captioned').forEach(handleInfo);
+    }
+
+    this.Figure = {
+        init: initialize
+    };
+}).call(window);
